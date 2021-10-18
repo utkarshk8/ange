f5a86196eb30662962ba91cff7a618207044c83a
utkarshk8 <91321060+utkarshk8@users.noreply.github.com>
2021-10-18 18:00:53 +0530

    Add files via upload

---
+++ src/Logo.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "../src/index.css";
+function Logo() {
+  return (
+  <div>
+    <div class="Parent">
+    <img src="https://i.ibb.co/Fnr0sDV/logo.png" alt="logo" />    </div>
+    <div class="Parent2">
+      <div class="child1">
+      <img src="https://i.ibb.co/jrkf3vK/1.png" alt="1" />
+      </div>
+      <div class="child2">
+        <div>
+          <span class="bold">C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</span>
+          <ul>
+            <li>
+              C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.
+            </li>
+            <li>
+              C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.
+            </li>
+          </ul>
+        </div>
+        <div>
+        <img src="https://i.ibb.co/bPXjdJ8/2.png" alt="2" />
+        </div>
+        <div class="text">
+        Government of India has awarded the <span class="bold">"National Energy Conservation Award 2018"</span> . Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.
+        </div>
+      </div>
+    </div>
+    <div class="Parent3">
+      <div class="">
+        INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.
+      </div>
+      <div class="">
+      <img src="https://i.ibb.co/5xCqZmT/3.png" alt="3" />
+      </div>
+      <div class="center">
+        Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors
+      </div>
+
+    </div>
+    <div class="linebreak">
+    </div>
+    <div class="Parent4 center bold">
+      C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS<br /><br />
+
+      CHEMICALS & PROCESS POWER WATER & WASTE WATER OILS & GAS PHARMA SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING FOOD & BEVERAGE<br /> PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE & RESIDENTIAL
+    </div>
+    <div class="Footer">
+      <div class="">
+      <img src="https://i.ibb.co/X3nkszX/call.png" alt="call" />Toll free 1800 200 1234
+      </div>
+      <div class="">
+        <a href="www.facebook.com/cripumps"><img src="https://i.ibb.co/6FyfGBJ/facebook-logo.png" alt="facebook-logo" />www.facebook.com/cripumps</a>
+      </div>
+      <div class="">
+        <a href="www.crigroups.com"><img src="https://i.ibb.co/Fnr0sDV/logo.png" alt="logo" />www.crigroups.com</a>
+      </div>
+    </div>
+  </div>);
+}
+export default Logo;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Logo from "./Logo";
+import "../src/index.css";
+
+ReactDOM.render(<Logo></Logo>, document.getElementById("root"));
